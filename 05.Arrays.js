c70a5cfbba175f0bbaa79a14c460c706ad9950fe
CSE <revengerreigns076@gmail.com>
2021-02-21 14:13:32 +0600

    Array functionalities

--- 05.Arrays.js
+++ 05.Arrays.js
@@ -1,1 +1,29 @@
 //Arrays
+// Initializing in good manner
+let seasons = []; // empty array
+let fruits = ["Mango", "Apple", "Banana"]; // array with elements
+
+//below process creates complexity and provide unexpected result
+let flowers = new Array("Rose", "Lily");
+
+// inserting elements into array
+seasons.push("Summer");
+seasons[1] = "Winter";
+
+// Accessing array elements
+console.log(seasons);
+console.log(seasons[0]);
+
+//keeping number,string,function,object etc inside array
+let keep = [
+  23,
+  "Ruman",
+  function () {
+    return "Bhuiyan";
+  },
+  { a: 1, b: 2 },
+];
+//accessing array elements
+console.log(keep[2]());
+console.log(keep[3].a);
+console.log(keep[3]["b"]); // object_name.key or object_name['key']
