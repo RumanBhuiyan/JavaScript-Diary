4a2c4d47e238f1e63a7885b1c207c50a108f2775
CSE <revengerreigns076@gmail.com>
2021-02-21 13:07:30 +0600

    Variables in javascript

---
+++ 01_Variables.js
@@ -0,0 +1,12 @@
+//Printing anything in javascript
+// use ; at end of statement or you can neglect it
+console.log("Hello world");
+
+/*Declaring variables in javascript
+variables naming procedure follows camelCase */
+var first_number = 4;
+let second_number = 5;
+
+//accessing variables value
+console.log(first_number);
+console.log(second_number);
