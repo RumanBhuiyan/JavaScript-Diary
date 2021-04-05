8e4dc6b802f07a5e47e1ad82248bcbbb9340e721
CSE <revengerreigns076@gmail.com>
2021-04-05 09:59:51 +0600

    Adding Certificate

--- 12.Arrow function.js
+++ 12.Arrow function.js
@@ -13,3 +13,16 @@ remainder = (a, b) => a % b;
 console.log(`Summation of 3,4 =${summation(3, 4)}`);
 console.log(`Product of 3,4 =${product(3, 4)}`);
 console.log(`Remainder of 4/3 =${remainder(4, 3)}`);
+
+//Nesting function which is seen only in javascript
+function addName(first_name) {
+    
+    function fullname(fname) {
+        
+        return `${fname} Bhuiyan`;
+    }
+
+    return fullname(first_name);
+}
+
+console.log(addName('Ruman'));
